e5cbb5730a9c86ecc18ecc316bba23d5270ba019
Rick <d287438@edu.curio.nl>
2023-09-26 13:39:54 +0200

    n

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
 <header>
     <div class="container">
         <nav>
-            <img src="<?php echo $base_url; ?>/img/logo-big-v4.png" alt="logo" class="logo">
+            <img src="<?php echo $base_url; ?>/img/logo-big-v4" alt="logo" class="logo">
             <a href="<?php echo $base_url; ?>/index.php">Home</a> |
             <a href="<?php echo $base_url; ?>/meldingen/index.php">Meldingen</a>
         </nav>
